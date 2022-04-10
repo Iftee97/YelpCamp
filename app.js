b80f3a3e0ff8e93062f154d789b5c664ce3f6860
Iftee1711402 <tawsifhaque97@gmail.com>
2022-04-11 05:03:24 +0600

    add start script

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-// http://localhost:3000
-app.listen(3000, () => {
-  console.log("server running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
